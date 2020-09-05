8a1a512bbc6806e5aa8a3ce6cedeb60148747a6a
Juande Martos <juande@bumxu.com>
2020-09-05 21:29:09 +0200

    PkFont doc.

--- src/engine/types/font/PkFont.ts
+++ src/engine/types/font/PkFont.ts
@@ -1,8 +1,21 @@
 import { DwContainer } from '@ng/drawable/dw/DwContainer';
 
+/**
+ * A font-like resource able to write text according to their characteristics.
+ */
 export interface PkFont {
-    
+    /**
+     * Width of each of the characters in this font.
+     */
     charWidth: number;
     
+    /**
+     * Write the specified text to a drawable container using this font.
+     *
+     * @param text   - Text to write.
+     * @param target - Target container to place the letters of the text. If not provided, a new one is created.
+     * @param x      - X coordinate to start placing the text; default is 0.
+     * @param y      - Y coordinate to start placing the text; default is 0.
+     */
     writeText(text: string, target?: DwContainer, x?: number, y?: number): DwContainer;
 }
